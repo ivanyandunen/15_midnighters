c3dec0c5e7b745786959083cc47cf8e204aaf463
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-02-06 18:50:02 +0300

    base added

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+This script takes information from [devman.org API](http://devman.org/api/challenges/solution_attempts)
+and filter people, who sent their solutions at midnight(time between 0:00 and 2:00)
+
+# How to use
+
+Python 3.5 or higher has to be installed and modules from requirements.txt
+You might have to run python3 instead of python depending on system. 
+
+```commandline
+python seek_dev_nighters.py
+```
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,60 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+from pytz import timezone
+from datetime import datetime, time
 
-def get_midnighters():
-    pass
+
+API_URL = 'https://devman.org/api/challenges/solution_attempts/'
+
+
+def get_number_of_pages():
+    first_page = 1
+    number_of_pages = requests.get(
+        API_URL,
+        params={'page': first_page}
+    ).json()['number_of_pages']
+    return number_of_pages
+
+
+def load_attempts(number_of_pages):
+    users_attempts = []
+    for page in range(1, number_of_pages):
+        users_attempts.extend(requests.get(
+            API_URL,
+            params={'page': page}
+        ).json()['records'])
+    return users_attempts
+
+
+def get_local_time(tz, timestamp):
+    user_timezone = timezone(tz)
+    time_from_timestamp = (datetime.fromtimestamp(timestamp))
+    user_local_time = user_timezone.fromutc(time_from_timestamp)
+    return user_local_time
+
+
+def get_midnighters(users_attempts):
+    midnight = time(2, 0)
+    midnighters = set()
+    for record in users_attempts:
+        user_local_time = get_local_time(
+            record['timezone'],
+            record['timestamp']
+        )
+        if user_local_time.time() < midnight:
+            midnighters.add(record['username'])
+    return midnighters
+
+
+def print_midnighters(midnighters):
+    if midnighters:
+        for midnighter in midnighters:
+            print(midnighter)
+    else:
+        print('No one pushed changes')
+
 
 if __name__ == '__main__':
-  pass
+    number_of_pages = get_number_of_pages()
+    users_attempts = load_attempts(number_of_pages)
+    midnighters = get_midnighters(users_attempts)
+    print_midnighters(midnighters)
