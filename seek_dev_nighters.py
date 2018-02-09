4caeaaa1b5be22dcdd46c570d3c4f4ecbe03b389
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-02-09 16:48:45 +0300

    for replaced with while in load_attempts() return replaced with yield in load_attemts() error with calculating local time fixed user_local_time.time() replaces with user_local_time.hour midnight = (2,0) -> midnight = 2 Global API_URL removed

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,52 +1,37 @@
 import requests
 from pytz import timezone
-from datetime import datetime, time
+from datetime import datetime
 
 
-API_URL = 'https://devman.org/api/challenges/solution_attempts/'
-
-
-def get_number_of_pages():
-    first_page = 1
-    number_of_pages = requests.get(
-        API_URL,
-        params={'page': first_page}
-    ).json()['number_of_pages']
-    return number_of_pages
-
-
-def load_attempts(number_of_pages):
-    users_attempts = []
-    for page in range(1, number_of_pages):
-        users_attempts.extend(requests.get(
-            API_URL,
+def load_attempts():
+    page = 1
+    while True:
+        users_attempts = requests.get(
+            'https://devman.org/api/challenges/solution_attempts/',
             params={'page': page}
-        ).json()['records'])
-    return users_attempts
-
-
-def get_local_time(tz, timestamp):
-    user_timezone = timezone(tz)
-    time_from_timestamp = (datetime.fromtimestamp(timestamp))
-    user_local_time = user_timezone.fromutc(time_from_timestamp)
-    return user_local_time
-
-
-def get_midnighters(users_attempts):
-    midnight = time(2, 0)
-    midnighters = set()
-    for record in users_attempts:
-        user_local_time = get_local_time(
-            record['timezone'],
-            record['timestamp']
         )
-        if user_local_time.time() < midnight:
-            midnighters.add(record['username'])
-    return midnighters
+
+        for attempt in users_attempts.json()['records']:
+            yield attempt
+
+        if page >= users_attempts.json()['number_of_pages']:
+            break
+        else:
+            page += 1
+
+
+def is_midnighter(attempt):
+    midnight = 2
+    user_local_time = datetime.fromtimestamp(
+        attempt['timestamp'],
+        timezone(attempt['timezone'])
+    )
+    return user_local_time.hour < midnight
 
 
 def print_midnighters(midnighters):
     if midnighters:
+        print('Midnigters are:')
         for midnighter in midnighters:
             print(midnighter)
     else:
@@ -54,7 +39,8 @@ def print_midnighters(midnighters):
 
 
 if __name__ == '__main__':
-    number_of_pages = get_number_of_pages()
-    users_attempts = load_attempts(number_of_pages)
-    midnighters = get_midnighters(users_attempts)
+    midnighters = set()
+    for attempt in load_attempts():
+        if is_midnighter(attempt):
+            midnighters.add(attempt['username'])
     print_midnighters(midnighters)
